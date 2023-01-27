659b1473a8703316c3bd5dd782ef6de3613524c2
Dac Anne Nguyen <nguyenanne066@gmail.com>
2023-01-27 11:10:06 -0500

    s1c2 personnalisation du thème

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <?php 
     if(have_posts()):
         while (have_posts()) : the_post();
-        the_title('<h1>','</h1>');
+        the_title('<h2>','</h2>');
         the_content(); 
 ?>
         <hr>
