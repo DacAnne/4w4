71c20e912d1bc948fbca4ca357efa5f2aa0c0303
Dac Anne Nguyen <nguyenanne066@gmail.com>
2023-02-13 13:45:23 -0500

    s4c1 Création de la branche lab3

--- front-page.php
+++ front-page.php
@@ -6,21 +6,23 @@
 ?>
 <?php get_header();?>
 <main>
-<?php 
-    if(have_posts()):
-        while (have_posts()) : the_post(); ?>
-    <h1>
-        <a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
-    </h1>
-    <?php //the_content();  // affiche le contenu complet de l'article ?>
-    <?php //the_excerpt(); // affiche résumé de l'article ?>
-    <?= wp_trim_words(get_the_excerpt(), 10, "&#8658;") ?>
-    <hr>
-        
-<?php    
-        endwhile;
-    endif;
-?>
+    <section class="blocflex">
+        <?php 
+            if(have_posts()):
+                while (have_posts()) : the_post(); ?>
+            <article>
+                <h2>
+                    <a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
+                </h2>
+                <?php //the_content();  // affiche le contenu complet de l'article ?>
+                <?php //the_excerpt(); // affiche résumé de l'article ?>
+                <p> <?= wp_trim_words(get_the_excerpt(), 10, "&#8658;") ?></p> 
+            </article>   
+        <?php    
+                endwhile;
+            endif;
+        ?>
+    </section>
     <!-- <h1> Bienvenue au cours de 4w4</h1> -->
 </main>
 <?php get_footer();?>
