38c43a2b799ab69888ecbb8c603dce976a70ff4a
Dac Anne Nguyen <nguyenanne066@gmail.com>
2023-02-20 13:46:24 -0500

    s4c1 Ajout d'un module de recherche et restylisation du menu

---
+++ search.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Modèle par défaut
+ * 
+ */
+?>
+<?php get_header();?>
+<main>
+    <h3>Search.php</h3>
+    <h2>Résultats de la recherche</h2>
+<?php 
+    if(have_posts()):
+        while (have_posts()) : the_post();
+        the_title('<h4>','</h4>');?>
+        
+        <?= wp_trim_words(get_the_excerpt(), 50, "[...]");?>
+        <hr>
+<?php    
+        endwhile;
+    endif;
+?>
+    <!-- <h1> Bienvenue au cours de 4w4</h1> -->
+</main>
+<?php get_footer();?>
