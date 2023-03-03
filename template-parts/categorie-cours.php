9abf10489db6e40dcd8eee3b8cbb07d36482030e
Dac Anne Nguyen <nguyenanne066@gmail.com>
2023-03-03 10:34:04 -0500

    s6c2 creation de template et incrémentation d'une gallerie

---
+++ template-parts/categorie-cours.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Template part pour afficher un blocflex article cours
+ */
+    $titre = get_the_title(); 
+    $sigle = substr($titre, 0, 7);
+    $titre_long = substr($titre,7,-5);
+    // $duree = "60"
+    $duree = substr($titre, strpos($titre, '('));
+    // strpost($titre, '(') permet de rouver la position du caractère. '('
+?>
+<article class="blocflex__article">
+        <h2><a href="<?php the_permalink(); ?>"> <?= $sigle ?></a></h2>
+        <h5><?= $titre_long ?></h5>
+        <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+        <h6><?= $duree ?></h6>
+
+</article>
---
+++ template-parts/categorie-galerie.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Template part pour afficher la galerie dans la page d'accueil
+ */
+   
+?>
+
+<article class="blockflex__galerie">
+    <?= the_content(); ?>
+</article>
