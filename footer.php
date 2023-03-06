454275669e7651772f543b56393c90d4ffd42852
Dac Anne Nguyen <nguyenanne066@gmail.com>
2023-03-06 13:51:49 -0500

    s7c1 personnalisation du footer et ajout de lien

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 <footer class="site__footer">
-<section>Colonne 1</section>
-<section>Colonne 2</section>
-<section>Colonne 3</section>
+<section><a href="https://github.com/DacAnne">Lien Github</a></section>
+<section><a href="https://www.behance.net/DacAnne">Lien Porfolio</a></section>
+<section><a href="https://cidweb14.sg-host.com/">Siteground</a></section>
 </footer>
 <?php wp_footer();?>
-  
 </body>
 </html>
