b285c6edc101c4527ad563488c12786a43b6ceb4
Dac Anne Nguyen <nguyenanne066@gmail.com>
2023-04-21 10:51:12 -0400

    s12c1 - enlèvement du section 3 du footer afin de les mettres par widgets dans le siteground

--- footer.php
+++ footer.php
@@ -2,12 +2,6 @@
     <section><div class="sidebar"><?php dynamic_sidebar( 'footer_1' ); ?></div></section>
     <section><a href="#"><div class="sidebar"><?php dynamic_sidebar( 'footer_2' ); ?></div></a></section>
     <section><div class="sidebar"><?php dynamic_sidebar( 'footer_3' ); ?></div></section>
-<!-- <ul> -->
-    <!-- <li><a href="https://github.com/DacAnne"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png"width="92" height="22" alt="Lien GithubLien Github"></a></li>
-    <li><a href="https://www.behance.net/DacAnne"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Behance_logo.svg/512px-Behance_logo.svg.png?20180331154838" width="92" height="22" alt="Lien Porfolio"></a></li>
-    <li><a href="https://cidweb14.sg-host.com/"><img src="https://world.siteground.com/img/downloads/siteground-logo-white-transparent-vector.svg" width="92" height="22" alt="Lien Porfolio"></a></li> -->
-<!-- </ul> -->
-
 </footer>
 <?php wp_footer();?>
 </body>
