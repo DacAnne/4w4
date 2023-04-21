97faae9a84e4b368d60d9c3f8416eb55af4dddea
Dac Anne Nguyen <nguyenanne066@gmail.com>
2023-04-21 10:25:14 -0400

    s12c1 optimisation du footer par widgets

--- footer.php
+++ footer.php
@@ -1,19 +1,13 @@
 <footer class="site__footer">
-    <section><div>
-        <h3>À propos</h3>
-                <p></p> 
-                <p>Thème réalisé par Nguyen, Dac Anne</p>
-                <p>4W4 - Création d'interface et développement Web - TIM 2023</p>
-            </div></section>
-    <section><a href="#"><img class="logo-secondaire" src="https://mir-s3-cdn-cf.behance.net/user/230/0a20a61480963353.63e81f50db12e.jpg" width="100" height="100" alt=""></a></section>
-<section>
-<ul>
-    <h3>réseaux sociaux</h2>
-    <li><a href="https://github.com/DacAnne"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png"width="92" height="22" alt="Lien GithubLien Github"></a></li>
+    <section><div class="sidebar"><?php dynamic_sidebar( 'footer_1' ); ?></div></section>
+    <section><a href="#"><div class="sidebar"><?php dynamic_sidebar( 'footer_2' ); ?></div></a></section>
+    <section><div class="sidebar"><?php dynamic_sidebar( 'footer_3' ); ?></div></section>
+<!-- <ul> -->
+    <!-- <li><a href="https://github.com/DacAnne"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png"width="92" height="22" alt="Lien GithubLien Github"></a></li>
     <li><a href="https://www.behance.net/DacAnne"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Behance_logo.svg/512px-Behance_logo.svg.png?20180331154838" width="92" height="22" alt="Lien Porfolio"></a></li>
-    <li><a href="https://cidweb14.sg-host.com/"><img src="https://world.siteground.com/img/downloads/siteground-logo-white-transparent-vector.svg" width="92" height="22" alt="Lien Porfolio"></a></li>
-</ul>
-</section>
+    <li><a href="https://cidweb14.sg-host.com/"><img src="https://world.siteground.com/img/downloads/siteground-logo-white-transparent-vector.svg" width="92" height="22" alt="Lien Porfolio"></a></li> -->
+<!-- </ul> -->
+
 </footer>
 <?php wp_footer();?>
 </body>
